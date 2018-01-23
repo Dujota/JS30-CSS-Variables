32496204d22bef0d42264cbfa5743a24dc130ad2
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-01-22 20:36:14 -0500

    fixed all the typos and added an event listener to the loop of items in the ctrols input node list

---
+++ scripts.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  const inputs = document.querySelectorAll('.controls input');
+
+  function handleUpdate() {
+    console.log(this.value);
+
+  }
+
+  inputs.forEach(input => input.addEventListener('change', handleUpdate));
+
+
+}, false);
