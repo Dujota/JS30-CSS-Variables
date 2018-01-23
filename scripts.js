f299cf8fb4733eaf2f6c9dc6129d9a27b157fa76
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-01-23 01:14:12 -0500

    added const suffix that grabs string px from the data- tags and returns '' for anything without a sizing value set the style property of this(input of inputs selected) to the value that is being listened to abnd append the suffix we got earlier

--- scripts.js
+++ scripts.js
@@ -3,7 +3,11 @@ document.addEventListener('DOMContentLoaded', function() {
   const inputs = document.querySelectorAll('.controls input');
 
   function handleUpdate() {
-    console.log(this.value);
+    const suffix = this.dataset.sizing || ''; // dataset is a list of all the data- in an object to be called and '' is for anything that doesnt have a suffix
+    console.log(this.suffix);
+
+    //selecting a variable, get the document and set a property(variable) and then append suffix that we grabbed from the data- tag 
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
 
   }
 
